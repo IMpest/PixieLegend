fa1a4b8b5245e8465d93ac1ceaed3a80b077d904
silver6wings <silver6wings@gmail.com>
2014-12-26 17:01:21 +0800

    添加了游戏原型v3.1

---
+++ Documents/游戏原型v3.1/files/plz_tell_me_your_name/data.js
@@ -0,0 +1,271 @@
+﻿$axure.loadCurrentPage({
+  "url":"plz_tell_me_your_name.html",
+  "generationDate":new Date(1419583432194.64),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable"],
+  "page":{
+    "packageId":"b77694e433a548678e886e2ba5bb07cc",
+    "type":"Axure:Page",
+    "name":"PLZ TELL ME YOUR NAME",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"Applied Font",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+},
+    "diagram":{
+      "objects":[{
+          "id":"1792834d7fb14560b0dcb30b9a1437fe",
+          "label":"手机",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "size":{
+              "width":754,
+              "height":1478}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"20182b0b75f34e73be8096b388602b1c",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "size":{
+                  "width":754,
+                  "height":1478}},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/plz_tell_me_your_name/手机_u0.png"}},
+{
+          "id":"8ff8084e9b8f4c7ca7acd1add96f8a5b",
+          "label":"",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":-99,
+              "y":419},
+            "size":{
+              "width":960,
+              "height":640},
+            "rotation":"90",
+            "textRotation":"90"},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"d6f4b8e8930c477094a9ef8aaa0cc926",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "location":{
+                  "x":-99,
+                  "y":419},
+                "size":{
+                  "width":960,
+                  "height":640},
+                "rotation":"90",
+                "textRotation":"90"},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/plz_tell_me_your_name/u2.jpg"}},
+{
+          "id":"2d9416a0a7484e58910fdb71c0cbe1cd",
+          "label":"",
+          "type":"buttonShape",
+          "styleType":"h1",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":86,
+              "y":396},
+            "size":{
+              "width":19,
+              "height":16}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"3da2f69715cd495dae1016f9f6eb4c28",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "location":{
+                  "x":86,
+                  "y":396},
+                "size":{
+                  "width":19,
+                  "height":16}},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"resources/images/transparent.gif"}},
+{
+          "id":"8ae8678053cb41a1a2f05a318e467408",
+          "label":"",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":181,
+              "y":610},
+            "size":{
+              "width":400,
+              "height":300}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"b49a4c2e3bd14f0d8c10c97df8da7be3",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "location":{
+                  "x":181,
+                  "y":610},
+                "size":{
+                  "width":400,
+                  "height":300}},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/plz_tell_me_your_name/u6.png"}},
+{
+          "id":"25db2bcadaaf4656acc4420e2c5fa9c5",
+          "label":"",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":269,
+              "y":920},
+            "size":{
+              "width":224,
+              "height":57}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"0db099a947c8434d814570cc3f7bf841",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "location":{
+                  "x":269,
+                  "y":920},
+                "size":{
+                  "width":224,
+                  "height":57}},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/plz_tell_me_your_name/u8.png"}},
+{
+          "id":"64f676bd80f64a34aeda90c1640126ee",
+          "label":"",
+          "type":"textBox",
+          "styleType":"textBox",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":281,
+              "y":780},
+            "size":{
+              "width":200,
+              "height":25}},
+          "adaptiveStyles":{
+}},
+{
+          "id":"63b0458647c94b37824d7c92be926a93",
+          "label":"",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":380,
+              "y":500},
+            "size":{
+              "width":50,
+              "height":50}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"dd26e57d4383406fba114c3a56d63248",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "location":{
+                  "x":380,
+                  "y":500},
+                "size":{
+                  "width":50,
+                  "height":50}},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/plz_tell_me_your_name/u11.png"}}]}},
+  "masters":{
+},
+  "objectPaths":{
+    "1792834d7fb14560b0dcb30b9a1437fe":{
+      "scriptId":"u0"},
+    "20182b0b75f34e73be8096b388602b1c":{
+      "scriptId":"u1"},
+    "8ff8084e9b8f4c7ca7acd1add96f8a5b":{
+      "scriptId":"u2"},
+    "d6f4b8e8930c477094a9ef8aaa0cc926":{
+      "scriptId":"u3"},
+    "2d9416a0a7484e58910fdb71c0cbe1cd":{
+      "scriptId":"u4"},
+    "3da2f69715cd495dae1016f9f6eb4c28":{
+      "scriptId":"u5"},
+    "8ae8678053cb41a1a2f05a318e467408":{
+      "scriptId":"u6"},
+    "b49a4c2e3bd14f0d8c10c97df8da7be3":{
+      "scriptId":"u7"},
+    "25db2bcadaaf4656acc4420e2c5fa9c5":{
+      "scriptId":"u8"},
+    "0db099a947c8434d814570cc3f7bf841":{
+      "scriptId":"u9"},
+    "64f676bd80f64a34aeda90c1640126ee":{
+      "scriptId":"u10"},
+    "63b0458647c94b37824d7c92be926a93":{
+      "scriptId":"u11"},
+    "dd26e57d4383406fba114c3a56d63248":{
+      "scriptId":"u12"}}});
